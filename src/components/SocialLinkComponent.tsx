7ddcb5d3a84393e8cd6bd4afc0f2e22eee1c35d5
Orhan Aytekin <contactorhanaytekin@gmail.com>
2024-09-02 14:42:27 +0300

    fix: ssr can not see window

--- src/components/SocialLinkComponent.tsx
+++ src/components/SocialLinkComponent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useEffect, useState } from 'react';
 import DraggableResizableComponent from './DraggableResizableComponent';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faMedium, faTwitter, faGoodreads, faLetterboxd, faSpotify, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
@@ -19,7 +20,17 @@ const icons = {
 
 const SocialLinkComponent = ({ platform, url }: { platform: keyof typeof icons | 'bento', url: string }) => {
   const IconComponent = icons[platform as keyof typeof icons];
-  const isDesktop = window.matchMedia("(min-width: 768px)").matches; // Check if screen width is at least 768px
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 768px)");
+    setIsDesktop(mediaQuery.matches);
+
+    const handleResize = () => setIsDesktop(mediaQuery.matches);
+    mediaQuery.addEventListener('change', handleResize);
+
+    return () => mediaQuery.removeEventListener('change', handleResize);
+  }, []);
 
   return (
     <DraggableResizableComponent>
