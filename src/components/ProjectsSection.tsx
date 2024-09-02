269d48b43805db0feef2b87e8b46e4428778c842
Orhan Aytekin <contactorhanaytekin@gmail.com>
2024-09-02 22:06:18 +0300

    fix: project links

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -7,19 +7,19 @@ const projects = [
     title: 'Lesion Meter',
     description: 'A mobile application designed to measure the area of skin lesions using images captured by mobile phones.',
     tech: 'Python, photogrammetry, image segmentation models (Segment Anything Model - SAM, adaptive thresholding), image processing',
-    link: 'https://github.com/orhanaytekin/lesion-meter',
+    links: ['https://github.com/orhanaytekin/LesionMeter'],
   },
   {
     title: 'Review and Recommendation System',
     description: 'A project that simulates a review and recommendation system for an e-commerce platform.',
     tech: 'React, Spring Boot, Hibernate, PostgreSQL',
-    link: 'https://github.com/orhanaytekin/review-recommendation-system',
+    links: ['https://github.com/orhanaytekin/bbm384-rrss-project-backend', 'https://github.com/orhanaytekin/bbm384-rrss-project-frontend'],
   },
   {
     title: 'BioMedical Data Mining',
     description: 'A project focused on extracting hidden trends and patterns from biomedical data for disease prediction.',
     tech: 'Python, LDA, scispacy, spacy, data analysis and visualization tools',
-    link: 'https://github.com/orhanaytekin/biomedical-data-mining',
+    links: ['https://github.com/orhanaytekin/biomedical-data-mining'],
   },
 ];
 
@@ -35,7 +35,21 @@ const ProjectsSection = () => {
             <h3 className="text-2xl mb-2">{project.title}</h3>
             <p className="mb-2">{project.description}</p>
             <p className="text-sm text-gray-400 mb-2">{project.tech}</p>
-            <a href={project.link} className="text-blue-500 hover:underline">View on GitHub</a>
+            <div className="mt-2">
+              {project.links.map((link, linkIndex) => (
+                <a
+                  key={linkIndex}
+                  href={link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline mr-4"
+                >
+                  {project.links.length === 2
+                    ? `View ${linkIndex === 0 ? 'Backend' : 'Frontend'} Source Code `
+                    : 'View Source Code'}
+                </a>
+              ))}
+            </div>
           </div>
         </DraggableResizableComponent>
       ))}
